40a36c5065eee1153387b759dc1c2c131274d35e
bulgariamitko <bulgaria_mitko@yahoo.com>
2017-03-03 21:17:50 +0200

    added sql file

--- db/connection.php
+++ db/connection.php
@@ -3,7 +3,7 @@ session_start();
 require 'functions.php';
 
 try {
-    $connection = new MysqlPDO('mysql:host=localhost; dbname=teamscorch; charset=utf8;', 'TeamScorch', 'softuni');
+    $connection = new MysqlPDO('mysql:host=localhost; dbname=TeamScorch; charset=utf8;', 'TeamScorch', 'softuni');
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
